82809b9851e84ad25036db86941c0edd0c4d4a4c
Homayoun <homayoun763@gmail.com>
2024-04-06 22:33:41 +0330

    add ads

--- app/page.jsx
+++ app/page.jsx
@@ -6,13 +6,17 @@ const Home = () => {
     <div>
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-0 md:col-span-2">
+          <div id="pos-article-display-95426"></div>
         </div>
         <div className="col-span-12 md:col-span-8">
           <Posts />
+          <div id="pos-article-display-95428"></div>
           <Posts />
+          <div id="pos-article-display-95428"></div>
           <Posts />
         </div>
         <div className="col-span-0 md:col-span-2">
+          <div id="pos-article-display-95427"></div>
         </div>
       </div>
     </div>
