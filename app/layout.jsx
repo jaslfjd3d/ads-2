deb8c3f885a2f936db0a0e1fee3b35e04a4ccf5c
jaslfjd3d <165952389+jaslfjd3d@users.noreply.github.com>
2024-04-07 19:56:26 +0330

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ const header = (
     <script
       dangerouslySetInnerHTML={{
         __html: `
-        !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/4dgHCENI/native-clicktlick.netlify.app-37203/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+            !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/XJi1izUj/native-clickplick.netlify.app-37219/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
             `,
       }}
     />
