6ef94e3f62cf9db3f510c86f747fdb42d74a5ea9
jaslfjd3d <165952389+jaslfjd3d@users.noreply.github.com>
2024-04-07 19:54:15 +0330

    Update Post.jsx

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -6,7 +6,7 @@ const Post = () => {
             <div className="card  bg-base-100 shadow-xl hover:scale-105 rounded-xl transition-transform ease-in-out duration-500 mb-11">
                 <figure className="px-5 pt-5">
                     <img
-                        src="/blog10.png"
+                        src="https://media.licdn.com/dms/image/D4D12AQHhJaOr-_E91A/article-cover_image-shrink_600_2000/0/1711904436166?e=2147483647&v=beta&t=xIDRalIv7K4iK23B0uDz0X0KPw3z9cAElo3NGxBtqS0"
                         className="rounded-xl aspect-video bg-gray-600 object-cover"
                     />
                 </figure>
