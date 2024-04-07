5e989c47fef4885cc6f98798bfafe99fcbe3f864
jaslfjd3d <165952389+jaslfjd3d@users.noreply.github.com>
2024-04-07 20:00:45 +0330

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,18 +6,18 @@ const Home = () => {
     <div>
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-0 md:col-span-2">
-          <div id="pos-article-display-95426"></div>
+         <div id="pos-article-display-95476"></div>
         </div>
         <div className="col-span-12 md:col-span-8">
           <Posts />
-          <div id="pos-article-display-95428"></div>
+         <div id="pos-article-text-95480"></div>
           <Posts />
-          <div id="pos-article-text-95472"></div>
+          <div id="pos-article-display-95481"></div>
           <Posts />
-          <div id="pos-article-display-95429"></div>
+          <div id="pos-article-text-95482"></div>
         </div>
         <div className="col-span-0 md:col-span-2">
-          <div id="pos-article-display-95427"></div>
+         <div id="pos-article-display-95477"></div>
         </div>
       </div>
     </div>
